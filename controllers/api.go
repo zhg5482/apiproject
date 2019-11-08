af720dd165bdc4e54588801a49ed0a0a05e6e450
张刚 <zg@bjadks.com>
2019-11-08 15:39:04 +0800

    update

--- controllers/api.go
+++ controllers/api.go
@@ -1,3 +1,16 @@
 package controllers
 
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+)
 
+type ApiController struct {
+	beego.Controller
+}
+
+func HttpSign() {
+	var u ApiController
+	//fmt.Println(string(u.Ctx.Input.RequestBody))
+	fmt.Println(u.Ctx.Request)
+}
